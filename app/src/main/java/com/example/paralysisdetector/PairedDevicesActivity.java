e6235fd710ef26966612439b25927b0f34544523
ongebo <isaacongebo@gmail.com>
2019-05-05 12:07:24 +0300

    Display the list of paired bluetooth devices

--- app/src/main/java/com/example/paralysisdetector/PairedDevicesActivity.java
+++ app/src/main/java/com/example/paralysisdetector/PairedDevicesActivity.java
@@ -1,7 +1,12 @@
 package com.example.paralysisdetector;
 
+import android.bluetooth.BluetoothDevice;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.ArrayList;
 
 public class PairedDevicesActivity extends AppCompatActivity {
 
@@ -9,5 +14,17 @@ public class PairedDevicesActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_paired_devices);
+
+        RecyclerView recyclerView;
+        Bundle bundle = getIntent().getExtras();
+        ArrayList<BluetoothDevice> pairedDevices;
+        pairedDevices = (ArrayList) bundle.getSerializable(
+                MainActivity.PAIRED_DEVICES
+        );
+
+        recyclerView = findViewById(R.id.recycler_view);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(new PairedDevicesAdapter(pairedDevices));
     }
 }
